b612810da3930f9cb92c0a58d808b2014adc41b8
idlePanic <hamidpahlevani1994@gmail.com>
2018-10-11 01:15:35 +0330

    change Roles method to roles :(

--- app/Permission.php
+++ app/Permission.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    public function Roles()
+    public function roles()
     {
         return $this->belongsToMany(Role::class);
     }
